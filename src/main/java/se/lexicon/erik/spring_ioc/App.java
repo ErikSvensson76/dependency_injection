c9509c0b492c75214d73c6bb81c5947768eec61b
Erik Svensson <eosvensson@gmail.com>
2019-09-03 11:00:48 +0200

    Added ComponentScan in XML and @Component annotation.

--- src/main/java/se/lexicon/erik/spring_ioc/App.java
+++ src/main/java/se/lexicon/erik/spring_ioc/App.java
@@ -1,9 +1,21 @@
 package se.lexicon.erik.spring_ioc;
 
+import java.time.LocalDate;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import se.lexicon.erik.spring_ioc.data.AppUserDao;
+import se.lexicon.erik.spring_ioc.model.AppUser;
 
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        ClassPathXmlApplicationContext context =
+        		new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+        AppUserDao dao = context.getBean(AppUserDao.class);
+        dao.addUser(new AppUser("Erik Svensson", LocalDate.now()));
+        System.out.println(dao.findById(1));
+        context.close();
     }
 }
--- src/main/java/se/lexicon/erik/spring_ioc/data/AppUserDaoListImpl.java
+++ src/main/java/se/lexicon/erik/spring_ioc/data/AppUserDaoListImpl.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Component;
+
 import se.lexicon.erik.spring_ioc.model.AppUser;
 
+@Component
 public class AppUserDaoListImpl implements AppUserDao {
 	
 	private List<AppUser> appUsers = new ArrayList<>();
